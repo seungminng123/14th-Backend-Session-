0f494e45e605c66c49a2ee4fe669618863c38324
seungminng <ssmmkim1025@naver.com>
2026-08-27 10:27:07 +0900

    :tada: Start: 바보

---
+++ src/main/java/com/likelion/besession/BeSessionApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.besession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/besession/BeSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.besession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
