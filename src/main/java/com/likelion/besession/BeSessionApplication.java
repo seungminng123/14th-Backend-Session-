cd8fbb1cd65badbf013527fd1cb7aa641506d23a
seungminng <ssmmkim1025@naver.com>
2026-08-31 10:55:52 +0900

    5주차 실습 및 과제

--- src/main/java/com/likelion/besession/BeSessionApplication.java
+++ src/main/java/com/likelion/besession/BeSessionApplication.java
@@ -2,8 +2,10 @@ package com.likelion.besession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //엔티티 생성일, 수정일 자동 기록0
 public class BeSessionApplication {
 
     public static void main(String[] args) {
